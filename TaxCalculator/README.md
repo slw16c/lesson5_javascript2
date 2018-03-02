af4f62bbbea8ac7f825ef05625106a564e9a73f1
Steve Walter <slw16c@my.fsu.edu>
2018-03-02 20:20:28 +0000

    Lesson 5 final commit

--- TaxCalculator/README.md
+++ TaxCalculator/README.md
@@ -4,20 +4,19 @@
 Web Application Development
 
 ### Assignment Date:  
-(Month Day, Year)
+March 2, 2018
 
 ### Student Name:  
-(First and Last Name)
+Steve Walter
 
 ### Project Description:
-(In your own words, briefly describe the assignment.)
+The assignment was to add the extra code to the .js file for the tax calculator to inclued: "Married filing jointly," "Married filing separately," and "Head of Household." also, to add extra code to clear the data after use.
 
 ### View Project:
-(Replace this statement with your Github Page URL that was created when you 
- published the project.)
+https://slw16c.github.io/lesson5_javascript2/
 
 ### Lessons Learned in the Assignment:
-1. (Briefly describe a lesson/concept learned in this lesson.)
-2. (Briefly describe a lesson/concept learned in this lesson.)
-3. (Briefly describe a lesson/concept learned in this lesson.)
+1. I learned that in the .getElementById, the E in .getElementById("taxEstimate") needs to be capitolized, because you're going to spend a couple hours trying to figure out why stuff isnt clearing. 
+2. I learned the ids in the HTML are important in showing the .getElementById where it needs to be. 
+3. I also learned that var means variable. 
 
--- TaxCalculator/taxBracket_Calculator.js
+++ TaxCalculator/taxBracket_Calculator.js
@@ -59,27 +59,47 @@ function calculateTax(){
                
           }//end of single filer calculation
           
-        
-        
-        
-          /*******************************************************************************
-             * Task 1:  Compute the tax for users' who file as "Married Jointly".  Also,
-             *          set (or initialize) the tax rate for each bracket.  Refer 
-             *          to the above example (single filer) and the assignment description
-             *          to write the correct equation for this filing status.
-             * 
-             * HINT:    If the "status" is equivalent to the value '1', the user is filing 
-             *          as "Married filing jointly or qualifying widow(er)".  
-             * 
-             *                       INSERT YOUR CODE BELOW!
-          *******************************************************************************/
-           
-           
            
           else if(status == 1) {
                 //INSERT YOUR CODE BELOW
-  
-  
+                
+                // Compute tax for married jointly filers
+               if (income <= 19050){
+                 tax = income * 0.10;
+                 taxRate = 10;
+               }//end of nested if
+               else if (income <= 77400) {
+                 tax = 19050 * 0.10 + (income - 19050) * 0.12;
+                 taxRate = 12;
+               }//end of else if
+               else if (income <= 82500) {
+                 tax = 19050 * 0.10 + (77400 - 19050) * 0.12 +
+                        (income - 77400) * 0.22;
+                 taxRate = 22;
+               } //end of else if
+               else if (income <= 315000) {
+                 tax = 19050 * 0.10 + (77400 - 19050) * 0.12 +
+                        (82500 - 77400) * 0.22 + (income - 82500) * 0.24;
+                 taxRate = 24;
+               }//end of else if
+               else if (income <= 400000) {
+                 tax = 19050 * 0.10 + (77400 - 19050) * 0.12 +
+                       (82500 - 77400) * 0.22 + (315000 - 82500) * 0.24 +
+                       (income - 315000) * 0.32;
+                 taxRate = 32;
+               }//end of else if
+               else if (income <= 600000) {
+                  tax = 19050 * 0.10 + (77400 - 19050) * 0.12 +
+                        (82500 - 77400) * 0.22 + (315000 - 82500) * 0.24 +
+                        (400000 - 315000) * 0.32 + (income - 400000) * 0.35;
+                  taxRate = 35;
+               }//end of else if
+               else {
+                  tax = 19050 * 0.10 + (77400 - 19050) * 0.12 +
+                        (82500 - 77400) * 0.22 + (315000 - 82500) * 0.24 +
+                        (400000 - 315000) * 0.32 + (600000 - 400000 ) * 0.35 + (income - 600000) * .37;
+                  taxRate = 37;
+               }
   
   
   
@@ -87,51 +107,90 @@ function calculateTax(){
           }//end of married file jointly calculation
           
           
-          
-          
-          /*******************************************************************************
-            * Task 2:  Compute the tax for users' who file as "Married Separately".  Also,
-            *          set (or initialize) the tax rate for each bracket. Refer 
-            *          to the above example (single filer) and the assignment description
-            *          to write the correct equation for this filing status.
-            * 
-            * HINT:    If the "status" is equivalent to the value '2', the user is filing 
-            *          as "Married filing separately."
-            * 
-            *                       INSERT YOUR CODE BELOW!
-          *******************************************************************************/
-                 
-                 
-                 
           else if(status == 2) {
                 //INSERT YOUR CODE BELOW
 
-             
+             // Compute tax for married separately filers
+               if (income <= 9525){
+                 tax = income * 0.10;
+                 taxRate = 10;
+               }//end of nested if
+               else if (income <= 38700) {
+                 tax = 9525 * 0.10 + (income - 9525) * 0.12;
+                 taxRate = 12;
+               }//end of else if
+               else if (income <= 82500) {
+                 tax = 9525 * 0.10 + (38700 - 9525) * 0.12 + (income - 38700) * 0.22;
+                 taxRate = 22;
+               } //end of else if
+               else if (income <= 157500) {
+                 tax = 9525 * 0.10 + (38700 - 9525) * 0.12 +
+                       (82500 - 38700) * 0.22 + (income - 82500) * 0.24;
+                 taxRate = 24;
+               }//end of else if
+               else if (income <= 200000) {
+                 tax = 9525 * 0.10 + (38700 - 9525) * 0.12 +
+                       (82500 - 38700) * 0.22 + (157500 - 82500) * 0.24 +
+                       (income - 157500) * 0.32;
+                 taxRate = 32;
+               }//end of else if
+               else if (income <= 300000) {
+                  tax = 9525 * 0.10 + (38700 - 9525) * 0.12 +
+                        (82500 - 38700) * 0.22 + (157500 - 82500) * 0.24 +
+                        (200000 - 157500) * 0.32 + (income - 200000) * 0.35;
+                  taxRate = 35;
+               }//end of else if
+               else {
+                  tax = 9525 * 0.10 + (38700 - 9525) * 0.12 +
+                        (82500 - 38700) * 0.22 + (157500 - 82500) * 0.24 +
+                        (200000 - 157500) * 0.32 + (300000 - 200000 ) * 0.35 + (income - 300000) * .37;
+                  taxRate = 37;
+               }//end of nested if
              
              
           }//end of married file separately calculation
         
-        
-        
-        
-          /*******************************************************************************
-            * Task 3:  Compute the tax for users' who file as "Head of Household".  Also,
-            *          set (or initialize) the tax rate for each bracket. Refer 
-            *          to the above example (single filer) and the assignment description
-            *          to write the correct equation for this filing status.
-            * 
-            * HINT:    If the "status" is equivalent to the value '3', the user is filing 
-            *          as "Head of Household."
-            * 
-            *                       INSERT YOUR CODE BELOW!
-          *******************************************************************************/
-          
-          
+
           
           else if(status == 3) {
                 //INSERT YOUR CODE BELOW
  
- 
+            // Compute tax for single filers
+               if (income <= 13600){
+                 tax = income * 0.10;
+                 taxRate = 10;
+               }//end of nested if
+               else if (income <= 51800) {
+                 tax = 13600 * 0.10 + (income - 13600) * 0.12;
+                 taxRate = 12;
+               }//end of else if
+               else if (income <= 82500) {
+                 tax = 13600 * 0.10 + (51800 - 13600) * 0.12 + (income - 51800) * 0.22;
+                 taxRate = 22;
+               } //end of else if
+               else if (income <= 157500) {
+                 tax = 13600 * 0.10 + (51800 - 13600) * 0.12 +
+                       (82500 - 51800) * 0.22 + (income - 82500) * 0.24;
+                 taxRate = 24;
+               }//end of else if
+               else if (income <= 200000) {
+                 tax = 13600 * 0.10 + (51800 - 13600) * 0.12 +
+                       (82500 - 51800) * 0.22 + (157500 - 82500) * 0.24 +
+                       (income - 157500) * 0.32;
+                 taxRate = 32;
+               }//end of else if
+               else if (income <= 500000) {
+                  tax = 13600 * 0.10 + (51800 - 13600) * 0.12 +
+                        (82500 - 51800) * 0.22 + (157500 - 82500) * 0.24 +
+                        (200000 - 157500) * 0.32 + (income - 200000) * 0.35;
+                  taxRate = 35;
+               }//end of else if
+               else {
+                  tax = 13600 * 0.10 + (51800 - 13600) * 0.12 +
+                        (82500 - 51800) * 0.22 + (157500 - 82500) * 0.24 +
+                        (200000 - 157500) * 0.32 + (500000 - 200000 ) * 0.35 + (income - 500000) * .37;
+                  taxRate = 37;
+               }//end of nested if
 
 
              
@@ -202,7 +261,9 @@ function clearButton () {
               
              //INSERT YOUR CODE BELOW
 
-
+            document.getElementById("taxEstimate").innerHTML = "";
+            document.getElementById("taxBracket").innerHTML = "";
+              
 
 
 
